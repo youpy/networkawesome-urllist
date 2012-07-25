d5989ffc901a5c8a20cc79787e03dfa21dd04c91
youpy <youpy@buycheapviagraonlinenow.com>
2012-07-26 07:33:35 +0900

    autoplay

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ $(function() {
   $('img').click(function() {
     var id = $(this).data('id');
     var iframe = $('<iframe>')
-      .attr('src', 'http://www.youtube.com/embed/' + id)
+      .attr('src', 'http://www.youtube.com/embed/' + id + '?autoplay=1')
       .attr('width', 459)
       .attr('height', 344)
       .attr('frameborder', 0)
